d552d4a44b0fbda45a3a1e3baa662a37e6f30e83
Gokul-31 <gokul.pg31@gmail.com>
2020-05-05 23:13:58 +0530

    Intent pass and Data transfer

---
+++ app/src/main/java/com/example/android/dotsandboxes/GridActivity.java
@@ -0,0 +1,31 @@
+package com.example.android.dotsandboxes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+public class GridActivity extends AppCompatActivity {
+
+    private String P1name;
+    private String P2name;
+    private int Size;
+    private TextView Check;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_grid);
+
+        Check=findViewById(R.id.checkText);
+
+        Intent gridActivity=getIntent();
+        P1name=gridActivity.getStringExtra("p1");
+        P2name=gridActivity.getStringExtra("p2");
+//        Size=Integer.parseInt(gridActivity.getStringExtra("Size"));
+        Size=gridActivity.getIntExtra("Size",5);
+        Check.setText(P1name+"   "+P2name+"  "+Size);
+    }
+}
